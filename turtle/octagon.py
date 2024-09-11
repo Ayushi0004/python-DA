608ad680e4b48e3e3f66c70acf67c52fcd6d93b1
ayushi0004 <ayushichaudhari404@gmail.com>
2024-09-11 11:24:22 +0530

    loops

---
+++ turtle/octagon.py
@@ -0,0 +1,24 @@
+# from turtle import *
+
+# forward(100)
+# left(45)
+# forward(100)
+# left(45)
+# forward(100)
+# left(45)
+# forward(100)
+# left(45)
+# forward(100)
+# left(45)
+# forward(100)
+# left(45)
+# forward(100)
+# left(45)
+# forward(100)
+
+#code using loops 
+from turtle import *
+for i in range(8):
+    forword = 100
+    left = 45
+mainloop()
